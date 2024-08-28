5f2bb2ec49243207b1aa9c1405ed6acf2b2a7d03
Chirag Agarwal <chirag@Chirags-MacBook-Air.local>
2024-08-28 11:52:12 +0530

    added some functionality in function bar

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,35 +1,43 @@
-import { useState } from "react";
 import "./Appbar.css";
 import smartsheets from "../assets/smartsheets.png";
+import { useNavigate } from "react-router-dom";
 
 function Appbar() {
+
+  const navigate = useNavigate();
+
+    const handleHome = () => {
+      navigate('/');
+    };
+
   return (
     <>
       <div className="navbar h-44">
         {/* logo */}
         <div className="div1">
           <img
+            onClick={handleHome}
             src={smartsheets}
             alt="smartsheets"
-            className="h-16 w-32 mt-10 ml-8"
+            className="cursor-pointer h-16 w-32 mt-10 ml-8"
           />
         </div>
 
         {/* profile */}
         <div className="div2">
-          <div className="rounded-full h-16 w-16 bg-[#EAF1FF] flex justify-center mt-10">
+          <button className="rounded-full h-16 w-16 bg-[#EAF1FF] flex justify-center mt-10">
             <div className="flex flex-col justify-center h-full text-xl">
               👤
             </div>
-          </div>
+          </button>
         </div>
 
         {/* tabs */}
         <div className="div3 flex gap-6">
-          <div className="flex flex-col h-full mr-8 mt-6">File</div>
-          <div className="flex flex-col h-full mr-8 mt-6">Edit</div>
-          <div className="flex flex-col h-full mr-8 mt-6">View</div>
-          <div className="flex flex-col h-full mr-8 mt-6">Insert</div>
+          <button className="flex flex-col h-full mr-8 mt-6">File</button>
+          <button className="flex flex-col h-full mr-8 mt-6">Edit</button>
+          <button className="flex flex-col h-full mr-8 mt-6">View</button>
+          <button className="flex flex-col h-full mr-8 mt-6">Insert</button>
         </div>
 
         {/* search */}
@@ -42,9 +50,9 @@ function Appbar() {
 
         {/* tabs */}
         <div className="div5 flex gap-6">
-          <div className="flex flex-col h-full mr-6 mt-6">Collaborate</div>
-          <div className="flex flex-col h-full mr-6 mt-6">Comments</div>
-          <div className="flex flex-col h-full mt-6">Share</div>
+          <button className="flex flex-col h-full mr-6 mt-6">Collaborate</button>
+          <button className="flex flex-col h-full mr-6 mt-6">Comments</button>
+          <button className="flex flex-col h-full mt-6">Share</button>
         </div>
 
         {/* main functions */}
